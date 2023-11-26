76ad8c8bb80c18a5648947da7032443c16c54cc5
Matt Granmoe <mattgranmoe@gmail.com>
2023-11-26 09:56:40 -0600

    Basic local dir scraping working

--- index.ts
+++ index.ts
@@ -1,14 +1,63 @@
-import axios from 'axios'
-import yargs from 'yargs'
 import fs from 'fs'
 import path from 'path'
+import axios from 'axios'
+import yargs from 'yargs'
+import ignore from 'ignore'
 
-async function fetchRepoContents(
+const isBinaryFile = (fileName: string): boolean => {
+  const binaryExtensions = [
+    '.png',
+    '.jpg',
+    '.jpeg',
+    '.gif',
+    '.bmp',
+    '.pdf',
+    '.zip',
+    '.exe',
+    '.dll',
+  ]
+
+  return binaryExtensions.some((extension) => fileName.endsWith(extension))
+}
+
+const isLockfile = (fileName: string): boolean => {
+  const lockfileNames = [
+    'package-lock.json',
+    'pnpm-lock.yaml',
+    'yarn.lock',
+    'Gemfile.lock',
+  ]
+
+  return lockfileNames.includes(fileName)
+}
+
+const ig = ignore()
+let hasInitializedIgnore = false
+
+const isGitIgnored = (filePath: string): boolean => {
+  if (!hasInitializedIgnore) {
+    hasInitializedIgnore = true
+
+    const gitignorePath = path.join(process.cwd(), '.gitignore')
+
+    if (fs.existsSync(gitignorePath)) {
+      const gitignore = fs.readFileSync(gitignorePath, 'utf8')
+      ig.add(gitignore)
+    }
+  }
+
+  // The ignore package requires relative paths
+  const relativePath = path.relative(process.cwd(), filePath)
+
+  return ig.ignores(relativePath)
+}
+
+const fetchRepoContents = async (
   owner: string,
   repo: string,
   path = '',
   page = 1,
-): Promise<FileInfo[]> {
+): Promise<FileInfo[]> => {
   const baseUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}?page=${page}`
   const response = await axios.get(baseUrl)
   const data = response.data
@@ -59,34 +108,7 @@ async function fetchRepoContents(
   return files
 }
 
-function isBinaryFile(fileName: string): boolean {
-  const binaryExtensions = [
-    '.png',
-    '.jpg',
-    '.jpeg',
-    '.gif',
-    '.bmp',
-    '.pdf',
-    '.zip',
-    '.exe',
-    '.dll',
-  ]
-
-  return binaryExtensions.some((extension) => fileName.endsWith(extension))
-}
-
-function isLockfile(fileName: string): boolean {
-  const lockfileNames = [
-    'package-lock.json',
-    'pnpm-lock.yaml',
-    'yarn.lock',
-    'Gemfile.lock',
-  ]
-
-  return lockfileNames.includes(fileName)
-}
-
-async function scrapeGitHubRepo(url: string): Promise<FileInfo[]> {
+const scrapeGitHubRepo = async (url: string): Promise<FileInfo[]> => {
   const regex = /github\.com\/(?<owner>[^\/]+)\/(?<repo>[^\/]+)/
   const match = url.match(regex)
 
@@ -99,31 +121,77 @@ async function scrapeGitHubRepo(url: string): Promise<FileInfo[]> {
   return fetchRepoContents(owner, repo)
 }
 
+const readFilesInDir = (dir: string, baseDir = dir): FileInfo[] => {
+  let results: FileInfo[] = []
+  const list = fs.readdirSync(dir)
+
+  list.forEach((file) => {
+    const filePath = path.resolve(dir, file)
+    const stat = fs.statSync(filePath)
+
+    if (isGitIgnored(filePath)) {
+      return
+    }
+
+    if (['.git', 'node_modules'].includes(file)) {
+      return
+    }
+
+    if (isBinaryFile(file) || isLockfile(file)) {
+      return
+    }
+
+    if (stat && stat.isDirectory()) {
+      // Recurse into a subdirectory
+      results = results.concat(readFilesInDir(filePath, baseDir))
+    } else {
+      const fileContent = fs.readFileSync(filePath, 'utf8')
+
+      results.push({
+        path: path.relative(baseDir, filePath),
+        url: `file://${filePath}`,
+        content: fileContent,
+      })
+    }
+  })
+
+  return results
+}
+
+const scrapeLocalDirectory = async (): Promise<FileInfo[]> => {
+  return readFilesInDir(process.cwd())
+}
+
 yargs(process.argv.slice(2))
   .scriptName('github-repo-gpt-scraper')
-  .usage('$0 <repo_url> --out <output_file>')
+  .usage('$0 --url <repo_url> --out <output_file>')
   .command(
-    '$0 <repo_url>',
+    '$0',
     false,
     (yargs) => {
       return yargs
-        .positional('repo_url', {
+        .option('url', {
           describe: 'URL of the GitHub repository to scrape',
           type: 'string',
-          demandOption: true,
         })
         .option('out', {
-          alias: 'o',
           describe: 'Output file name (will write to command directory)',
           type: 'string',
           demandOption: true,
         })
     },
     async (argv) => {
-      const repoUrl = argv.repo_url
+      const url = argv.url
       const outputPath = argv.out
 
-      const files = await scrapeGitHubRepo(repoUrl)
+      let files
+      if (url) {
+        console.log(`Scraping GitHub repository from URL ${url}`)
+        files = await scrapeGitHubRepo(url)
+      } else {
+        console.log(`Scraping local repo within dir ${process.cwd()}`)
+        files = await scrapeLocalDirectory()
+      }
 
       // Write files to disk at the specified output path
       const outputFilePath = path.join(process.cwd(), outputPath)
